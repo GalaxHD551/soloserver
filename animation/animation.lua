76e1b13e78cd953fdd66be328a14e25507610d74
GalaxHD551 <59099660+GalaxHD551@users.noreply.github.com>
2019-12-21 17:19:29 +0400

    Add files via upload

---
+++ animation/animation.lua
@@ -0,0 +1,165 @@
+local animID = {}
+animID[0] = { name="STOP", id=0 };
+animID[1] = { name="COMBINE", id=1 };
+animID[2] = { name="PICKUP_LOWER", id=2 };
+animID[3] = { name="PICKUP_MIDDLE", id=3 };
+animID[4] = { name="PICKUP_UPPER", id=4 };
+animID[5] = { name="HANDSHEAD_KNEEL", id=5 };
+animID[6] = { name="HANDSHEAD_STAND", id=6 };
+animID[7] = { name="HANDSUP_KNEEL", id=7 };
+animID[8] = { name="HANDSUP_STAND", id=8 };
+animID[9] = { name="ENTERCODE", id=9 };
+animID[10] = { name="VOMIT", id=10 };
+animID[11] = { name="CROSSARMS", id=11 };
+animID[12] = { name="DABSAREGAY", id=12 };
+animID[13] = { name="DONTKNOW", id=13 };
+animID[14] = { name="DUSTOFF", id=14 };
+animID[15] = { name="FACEPALM", id=15 };
+animID[16] = { name="IDONTLISTEN", id=16 };
+animID[17] = { name="FLEXX", id=17 };
+animID[18] = { name="HALTSTOP", id=18 };
+animID[19] = { name="INEAR_COMM", id=19 };
+animID[20] = { name="ITSJUSTRIGHT", id=20 };
+animID[21] = { name="FALLONKNEES", id=21 };
+animID[22] = { name="KUNGFU", id=22 };
+animID[23] = { name="CALLME", id=23 };
+animID[24] = { name="SALUTE", id=24 };
+animID[25] = { name="SHOOSH", id=25 };
+animID[26] = { name="SLAPOWNASS", id=26 };
+animID[27] = { name="SLAPOWNASS2", id=27 };
+animID[28] = { name="THROATSLIT", id=28 };
+animID[29] = { name="THUMBSUP", id=29 };
+animID[30] = { name="WAVE3", id=30 };
+animID[31] = { name="WIPEOFFSWEAT", id=31 };
+animID[32] = { name="KICKDOOR", id=32 };
+animID[33] = { name="LOCKDOOR", id=33 };
+animID[34] = { name="CRAZYMAN", id=34 };
+animID[35] = { name="DARKSOULS", id=35 };
+animID[36] = { name="SMOKING", id=36 };
+animID[37] = { name="CLAP", id=37 };
+animID[38] = { name="SIT01", id=38 };
+animID[39] = { name="SIT02", id=39 };
+animID[40] = { name="SIT03", id=40 };
+animID[41] = { name="SIT04", id=41 };
+animID[42] = { name="SIT05", id=42 };
+animID[43] = { name="SIT06", id=43 };
+animID[44] = { name="SIT07", id=44 };
+animID[45] = { name="LAY01", id=45 };
+animID[46] = { name="LAY02", id=46 };
+animID[47] = { name="LAY03", id=47 };
+animID[48] = { name="LAY04", id=48 };
+animID[49] = { name="LAY05", id=49 };
+animID[50] = { name="LAY06", id=50 };
+animID[51] = { name="LAY07", id=51};
+animID[52] = { name="LAY08", id=52 };
+animID[53] = { name="LAY09", id=53 };
+animID[54] = { name="LAY010", id=54 };
+animID[55] = { name="LAY011", id=55 };
+animID[56] = { name="LAY012", id=56 };
+animID[57] = { name="LAY013", id=57 };
+animID[58] = { name="LAY014", id=58 };
+animID[59] = { name="LAY015", id=59 };
+animID[60] = { name="LAY016", id=60 };
+animID[61] = { name="LAY017", id=61 };
+animID[62] = { name="LAY018", id=62 };
+animID[63] = { name="WAVE", id=63 };
+animID[64] = { name="WAVE2", id=64 };
+animID[65] = { name="STRETCH", id=65 };
+animID[66] = { name="BOW", id=66 };
+animID[67] = { name="CALL_GUARDS", id=67 };
+animID[68] = { name="CALL_SOMEONE", id=68 };
+animID[69] = { name="CALL_SOMEONE2", id=69 };
+animID[70] = { name="CHECK_EQUIPMENT", id=70 };
+animID[71] = { name="CHECK_EQUIPMENT2", id=71 };
+animID[72] = { name="CHECK_EQUIPMENT3", id=72 };
+animID[73] = { name="CLAP2", id=73 };
+animID[74] = { name="CLAP3", id=74 };
+animID[75] = { name="CHEER", id=75 };
+animID[76] = { name="DRUNK", id=76 };
+animID[77] = { name="FIX_STUFF", id=77 };
+animID[78] = { name="GET_HERE", id=78 };
+animID[79] = { name="GET_HERE2", id=79 };
+animID[80] = { name="GOAWAY", id=80 };
+animID[81] = { name="LAUGH", id=81 };
+animID[82] = { name="SALUTE2", id=82 };
+animID[83] = { name="THINKING", id=83 };
+animID[84] = { name="THROW", id=84 };
+animID[85] = { name="TRIUMPH", id=85 };
+animID[86] = { name="WASH_WINDOWS", id=86 };
+animID[87] = { name="WATCHING", id=87 };
+animID[88] = { name="DANCE01", id=88 };
+animID[89] = { name="DANCE02", id=89 };
+animID[90] = { name="DANCE03", id=90 };
+animID[91] = { name="DANCE04", id=91 };
+animID[92] = { name="DANCE05", id=92 };
+animID[93] = { name="DANCE06", id=93 };
+animID[94] = { name="DANCE07", id=94 };
+animID[95] = { name="DANCE08", id=95 };
+animID[96] = { name="DANCE09", id=96 };
+animID[97] = { name="DANCE10", id=97 };
+animID[98] = { name="DANCE11", id=98 };
+animID[99] = { name="DANCE12", id=99 };
+animID[100] = { name="DANCE13", id=100 };
+animID[101] = { name="DANCE14", id=101 };
+animID[102] = { name="DANCE15", id=102 };
+animID[103] = { name="DANCE16", id=103 };
+animID[104] = { name="DANCE17", id=104 };
+animID[105] = { name="DANCE18", id=105 };
+animID[106] = { name="DANCE19", id=106 };
+animID[107] = { name="DANCE20", id=107 };
+animID[108] = { name="CUFF", id=108 };
+animID[109] = { name="CUFF2", id=109 };
+animID[110] = { name="REVIVE", id=110 };
+animID[111] = { name="PICKAXE_SWING", id=111 };
+animID[112] = { name="CROSSARMS2", id=112 };
+animID[113] = { name="BARCLEAN01", id=113 };
+animID[114] = { name="BARCLEAN02", id=114 };
+animID[115] = { name="PHONE_PUTAWAY", id=115 };
+animID[116] = { name="PHONE_TAKEOUT", id=116 };
+animID[117] = { name="PHONE_TALKING01", id=117 };
+animID[118] = { name="PHONE_TALKING02", id=118 };
+animID[119] = { name="PHONE_TALKING03", id=119 };
+animID[120] = { name="DRINKING", id=120 };
+animID[121] = { name="SHRUG", id=121 };
+animID[122] = { name="SMOKING01", id=122 };
+animID[123] = { name="SMOKING02", id=123 };
+animID[124] = { name="SMOKING03", id=124 };
+animID[125] = { name="THINKING01", id=125 };
+animID[126] = { name="WALLLEAN01", id=126 };
+animID[127] = { name="WALLLEAN02", id=127 };
+animID[128] = { name="WALLLEAN03", id=128 };
+animID[129] = { name="WALLLEAN04", id=129 };
+animID[130] = { name="YAWN", id=130 };
+animID[131] = { name="FISHING", id=131 };
+animID[132] = { name="PHONE_TAKEOUT_HOLD", id=132 };
+animID[133] = { name="PHONE_HOLD", id=133};
+animID[134] = { name="SHOUT01", id=134 };
+animID[135] = { name="CART_IDLE", id=135 };
+animID[136] = { name="CARRY_IDLE", id=136 };
+animID[137] = { name="CARRY_SETDOWN", id=137};
+animID[138] = { name="CARRY_SHOULDER_IDLE", id=138 };
+animID[139] = { name="CARRY_SHOULDER_SETDOWN", id=139 };
+animID[140] = { name="HANDSHAKE", id=140 };
+animID[141] = { name="PUSHUP_START", id=141 };
+animID[142] = { name="PUSHUP_IDLE", id=142 };
+animID[143] = { name="PUSHUP_END", id=143 };
+animID[144] = { name="SLAP01", id=144 };
+animID[145] = { name="SLAP01_REACT", id=145 };
+
+function cmd_a(playerid, index)
+    if (index == nil) then
+        return AddPlayerChat(playerid, "Usage: /a <animation>")
+    end
+
+    index = tonumber(index)
+
+    if (index < 0 or index > 146) then
+        return AddPlayerChat(playerid, "Animation "..index.." does not exist.")
+    end
+
+    -- Set the animation
+    SetPlayerAnimation(playerid, animID[index].name)
+
+    AddPlayerChat(playerid, "Doing the animation! ( ID: "..index.." "..animID[index].name..")")
+end
+AddCommand("a", cmd_a)
