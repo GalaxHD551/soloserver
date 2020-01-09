147bb6bde86393893fb2676dd6287c35c87a6aed
GalaxHD551 <59099660+GalaxHD551@users.noreply.github.com>
2020-01-10 02:17:09 +0400

    Update client.lua

--- dropgun/client.lua
+++ dropgun/client.lua
@@ -1,7 +1,3 @@
---[[slotequiped = GetPlayerEquippedWeaponSlot(playerid)
-gun = GetPlayerWeapon(slotequiped)]]
-
-
 AddEvent("OnKeyPress", function(key)
     if key == "X" and not onSpawn and not onCharacterCreation then
         CallRemoteEvent("DropGun")        
