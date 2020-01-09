1896e83833b45f495b199b75f0aab0ce85dc0a14
GalaxHD551 <59099660+GalaxHD551@users.noreply.github.com>
2020-01-10 02:16:18 +0400

    Add files via upload

---
+++ dropgun/client.lua
@@ -0,0 +1,15 @@
+--[[slotequiped = GetPlayerEquippedWeaponSlot(playerid)
+gun = GetPlayerWeapon(slotequiped)]]
+
+
+AddEvent("OnKeyPress", function(key)
+    if key == "X" and not onSpawn and not onCharacterCreation then
+        CallRemoteEvent("DropGun")        
+    end
+end)
+
+AddEvent("OnKeyPress", function(key)
+    if key == "E" and not onSpawn and not onCharacterCreation then
+        CallRemoteEvent("Pick")
+    end
+end)
